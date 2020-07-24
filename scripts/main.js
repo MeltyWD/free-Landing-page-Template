8577d8bcc5cfdeed56e9bf1df0d27e5f9d2cbaac
Melty <Meltygd@gmail.com>
2020-07-24 20:55:42 +0900

    Добавлено фон и скрол кнопки меню

--- scripts/main.js
+++ scripts/main.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 });
 
 // Плавный скролл
-$(document).ready(function () {
+$(document).ready(function() {
   $(document).on("scroll", onScroll);
 
   $('a[href^="#"]').on('click', function (e) {
@@ -49,3 +49,36 @@ function onScroll(event){
       }
   });
 }
+
+
+$(document).ready(function(){ //Вызов функции по загрузке интерфейса
+  var tempScrollTop, currentScrollTop = $(window).scrollTop(); //объявление переменных и присвоение им значений
+  $(window).scroll(function(){ //Вызов функции при прокрутке страницы
+    currentScrollTop = $(window).scrollTop(); //присвоение переменной нового значения
+      if (currentScrollTop > $('header__burger-circle').height()) { //Проверка условия 'переменная больше высоты шапки'
+        $('root').addClass('fixed-header'); // создание класса 'fixed-header' в селекторе 'body'
+        if ( tempScrollTop > currentScrollTop ) { //Проверка условия 'значение переменной до вызова функции больше значения после её вызова'
+          $('header__burger-circle').addClass('show'); //создание класса 'show' в селекторе 'header'
+        } else { // выполнение, если второе условие не прошло проверку
+          $('header__burger-circle').removeClass('show'); //удаление класса 'show' в селекторе 'header'
+        }
+      } else { // выполнение, если первое условие не прошло проверку
+        $('root').removeClass('fixed-header'); // удаление класса 'fixed-header' в селекторе 'body'
+        $('header__burger-circle').removeClass('show'); //удаление класса 'show' в селекторе 'header'
+      }
+        tempScrollTop = currentScrollTop; //присвоение одной переменной значение другой
+  });
+});
+
+
+// Сдвиг кнопки меню в верх при скроле
+// p.s сам сделал =(
+$(document).ready(function(){
+  var paralax = document.getElementById("paralax");
+  $(window).scroll(function(){
+    paralax.style.top = 48 + pageYOffset * -0.2 + "px";
+    if (paralax.style.top < "0px") {
+      paralax.style.top = "0px"
+    }
+  });
+});
